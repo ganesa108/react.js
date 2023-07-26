ad3af54109eb6495aad5f3b0c179912601d98d8d
shivani3301 <ksweedan7@icloud.com>
2023-07-26 14:29:53 +0300

    react.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { Fragment } from "react";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
-<Fragment>
-  <div className="App">
-    <Card>
-      <Card.Body>
-        <Card.Title>Your Favourite Character</Card.Title>
-        <Card.Text>please select your favourite character :p</Card.Text>
-      </Card.Body>
-    </Card>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </div>
-</Fragment>;
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Card>
+          <Card.Body>
+            <Card.Title>Your Favourite Character</Card.Title>
+            <Card.Text>please select your favourite character :p</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+}
