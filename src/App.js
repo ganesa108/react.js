03bcbf00b695645825d3367aff9e518b8f721aec
shivani3301 <ksweedan7@icloud.com>
2023-07-27 15:15:34 +0300

    react.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ export default function App() {
         {/*navigation bar*/}
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="#home">Mortal Kombat X</Navbar.Brand>
+            <Navbar.Brand href="#home" style={{ fontWeight: "bold" }}>
+              Mortal Kombat X
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
@@ -30,37 +32,46 @@ export default function App() {
         {/*header*/}
         <Card>
           <Card.Body>
-            <Card.Title>
+            <Card.Title style={{ textAlign: "center", fontWeight: "bold" }}>
               Which one of us is your favourite MKX character?
             </Card.Title>
           </Card.Body>
         </Card>
         {/*card one*/}
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={subzero1} alt="sub" />
-          <Card.Body>
-            <Card.Title className="subzero">SUB-ZERO</Card.Title>
-            <Card.Text>
-              Be stealthful as the night...and deadly as the dawn.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        {/*card two*/}
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={mileena2} alt="mileena" />
-          <Card.Body>
-            <Card.Title className="mileena">MILEENA</Card.Title>
-            <Card.Text>Tasty!</Card.Text>
-          </Card.Body>
-        </Card>
-        {/*card three*/}
-        <Card style={{ width: "18rem" }}>
-          <img variant="top" src={scorpion3} alt="scorpion" />
-          <Card.Body>
-            <Card.Title className="scorpion">SCORPION</Card.Title>
-            <Card.Text>Vengeance will be mine!</Card.Text>
-          </Card.Body>
-        </Card>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "rows",
+            justifyContent: "space-around",
+            padding: 100,
+          }}
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={subzero1} alt="sub" />
+            <Card.Body>
+              <Card.Title className="subzero">SUB-ZERO</Card.Title>
+              <Card.Text>
+                Be stealthful as the night...and deadly as the dawn.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          {/*card two*/}
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={mileena2} alt="mileena" />
+            <Card.Body>
+              <Card.Title className="mileena">MILEENA</Card.Title>
+              <Card.Text>Tasty!</Card.Text>
+            </Card.Body>
+          </Card>
+          {/*card three*/}
+          <Card style={{ width: "18rem" }}>
+            <img variant="top" src={scorpion3} alt="scorpion" />
+            <Card.Body>
+              <Card.Title className="scorpion">SCORPION</Card.Title>
+              <Card.Text>Vengeance will be mine!</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
         {/*use of forms checkpoint objective; select list.*/}
         <Form.Select aria-label="Default select example">
           <option>select your favourite character here</option>
